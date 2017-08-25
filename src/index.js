8d7c51cc1eec9fe74492044ff64da59162997aa5
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-08-25 13:00:17 -0600

    youtube api returned data and added to state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/searchBar';
 
@@ -7,16 +8,23 @@ const lame = require('../lame.js');
 
 const API_KEY = lame.APIkey;
 
-// Create a new component.  This component should produce
-// some html.
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { vieos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+        <div>
+            <SearchBar />
+        </div>
+        );
+    }
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM).
 ReactDOM.render(<App />, document.querySelector('.container'));

dc4bfbf0d233460b533319d93e1a61c24e375904
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-08-25 12:08:11 -0600

    Search bar started

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/searchBar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const lame = require('../lame.js');
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = lame.APIkey;
+
+// Create a new component.  This component should produce
+// some html.
+const App = () => {
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
